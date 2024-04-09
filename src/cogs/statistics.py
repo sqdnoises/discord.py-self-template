91b674b972e05c8c2220e5e75ed214467d07b3be
sqdnoises <sqdnoises@gmail.com>
2024-04-09 06:46:06 +0530

    a lot

--- src/cogs/statistics.py
+++ src/cogs/statistics.py
@@ -1,4 +1,7 @@
+import time
 import asyncio
+
+import objects
 from classes import Bot, Context
 
 import discord
@@ -18,23 +21,87 @@ class Statistics(commands.Cog):
         )
 
         return statistics
+
+    async def get_all_statistics(self):
+        prisma = self.bot.prisma
+        
+        statistics = await prisma.statistics.find_many()
+        stats = {
+            "commands_used": sum([stat.commands_used for stat in statistics])
+        }
+        
+        return objects.Statistics(**stats)
+    
+    async def measure_ping(self, ctx: Context, message: str = "Ping?"):
+        """Measure the ping"""
+        start = time.monotonic()
+        msg = await ctx.send(message)
+        end = time.monotonic()
+
+        return msg, end - start
+    
+    @commands.command(aliases=["latency"])
+    async def ping(self, ctx: Context):
+        """Check the latency of the bot"""
+        bot = self.bot
+        msg, msg_latency = await self.measure_ping(ctx)
+        await msg.edit(content=f"Pong! 🏓 (took `{round(msg_latency * 1000)}ms`, ws: `{round(bot.latency * 1000)}ms`)")
     
     @commands.command(aliases=["stats"])
     async def statistics(self, ctx: Context):
         """Statistics of the bot"""
-        
-        await asyncio.sleep(1)
+        bot = self.bot
         
         stats = await self.get_statistics(ctx.guild.id)
+        all_stats = await self.get_all_statistics()
         
         embed = discord.Embed(
-            title = "Statistics",
-            description = f"**Commands used: ** {stats.commands_used}",
+            title = f"🌟 {ctx.me.display_name} Statistics",
             timestamp = discord.utils.utcnow(),
             color=discord.Color.blurple()
         )
         
-        await ctx.send(embed=embed)
+        embed.add_field(
+            name = "Servers",
+            value = f"`{len(bot.guilds)}`"
+        )
+        
+        embed.add_field(
+            name = "Users",
+            value = f"`{len(bot.users)}`"
+        )
+        
+        embed.add_field(
+            name = "Commands used",
+            value = f"`{stats.commands_used}` (server)\n"
+                    f"`{all_stats.commands_used}` (total)"
+        )
+        
+        embed.add_field(
+            name = "Uptime",
+            value = f"up since <t:{int(bot.uptime.timestamp())}:R>"
+        )
+        
+        msg, msg_latency = await self.measure_ping(ctx)
+        
+        embed.add_field(
+            name = "Latency",
+            value = f"`{round(msg_latency * 1000)}ms` (message)\n"
+                    f"`{round(bot.latency * 1000)}ms` (websocket)"
+        )
+        
+        embed.set_footer(
+            text = f"Requested by {ctx.author.display_name}",
+            icon_url = ctx.author.display_avatar
+        )
+        
+        embed.set_thumbnail(
+            url = bot.user.display_avatar
+        )
+        
+        await msg.edit(content="Pong! 🏓", embed=embed)
+        await asyncio.sleep(3)
+        await msg.edit(content="")
 
 async def setup(bot: Bot) -> None:
     await bot.add_cog(Statistics(bot))
